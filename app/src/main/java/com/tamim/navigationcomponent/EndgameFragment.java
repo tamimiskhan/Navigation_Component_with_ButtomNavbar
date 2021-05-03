ac91a04339b75aae220f40d01e9bcc3de7be76fa
Tamim <tamimiskhan@gmail.com>
2021-05-03 23:59:08 +0600

    Initial Commit

---
+++ app/src/main/java/com/tamim/navigationcomponent/EndgameFragment.java
@@ -0,0 +1,46 @@
+package com.tamim.navigationcomponent;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+
+public class EndgameFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_endgame, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        Button button = view.findViewById(R.id.restartGameButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                navController.navigate(R.id.action_endgameFragment_to_startFragment);
+
+                NavOptions navOptions = new NavOptions.Builder().setPopUpTo(R.id.startFragment, true).build();
+
+                navController.navigate(R.id.action_endgameFragment_to_startFragment,null,navOptions);
+
+            }
+        });
+
+    }
+}
